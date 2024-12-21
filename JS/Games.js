cbf960980f7c0adb60e9b225169e1abd3d7a65e5
Isco102345 <miseesmall30@gmail.com>
2024-12-21 18:34:01 +0200

    sec commit

--- JS/Games.js
+++ JS/Games.js
@@ -1,70 +1,4 @@
-/* let loading = document.getElementById("loading")
-let navLinks = document.querySelectorAll(".navbar-nav .nav-link"); 
-
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", function (e) {
-        e.preventDefault(); 
-        let category = e.target.getAttribute("data-category"); 
-        getGames(category); 
-        for (let j = 0; j < navLinks.length; j++) {
-            navLinks[j].classList.replace("text-info","text-white");
-        }
-        e.target.classList.replace("text-white","text-info");
-
-    });
-}
-
-getGames("mmorpg")
-
-let allGames = []
-
-async function getGames(category) {
-    let options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': '8790f1dafamsh856b2e63bc188c7p162535jsn887206772f31',
-            'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
-        }
-    };
-    loading.classList.remove("d-none")
-    let api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`, options)
-    let response = await api.json()
-    allGames = response;
-
-    disblayData()
-    loading.classList.add("d-none")
-}
-function disblayData() {
-    let cartona = "";
-
-    for (let i = 0; i < allGames.length; i++) {
-        cartona += `
-       <div class="col-md-6 col-lg-3">
-            <div class="card bg-transparent h-100" role="button"  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails(${allGames[i].id})">
-                            <div class="card-img-top img-container">
-                                <img src="${allGames[i].thumbnail}" alt="Tarisland" class="img-fluid">
-                            </div>
-                            <div class="card-body text-center d-flex justify-content-between">
-                                <h6 class="fw-bold text-white">${allGames[i].title}</h6>
-                                <span class="badge-span ">Free</span>
-                            </div>
-                            <p class="card-text small text-center">
-                                ${allGames[i].short_description}
-                            </p>
-                            <footer class="card-footer d-flex justify-content-between">
-                                <span class="badge badge-color">${allGames[i].publisher.split(" " , 2).join( " ")}</span>
-                                <span class="badge badge-color">${allGames[i].platform}</span>
-                            </footer>
-            </div>
-        </div>
-       `
-    }
-    document.getElementById("games-row").innerHTML =cartona;
-}
- */
-
-
-export class GamePlayList {
+class GamePlayList {
     constructor() {
         this.apiOptions = {
             method: 'GET',
@@ -148,7 +82,83 @@ export class GamePlayList {
     
 
 }
-// let gamePlayList = new GamePlayList();
+let gamePlayList = new GamePlayList();
+
+
+
+
+
+
+
+
+
+/****************Comments********************/
+
+/* let loading = document.getElementById("loading")
+let navLinks = document.querySelectorAll(".navbar-nav .nav-link"); 
+
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function (e) {
+        e.preventDefault(); 
+        let category = e.target.getAttribute("data-category"); 
+        getGames(category); 
+        for (let j = 0; j < navLinks.length; j++) {
+            navLinks[j].classList.replace("text-info","text-white");
+        }
+        e.target.classList.replace("text-white","text-info");
+
+    });
+}
+
+getGames("mmorpg")
+
+let allGames = []
+
+async function getGames(category) {
+    let options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '8790f1dafamsh856b2e63bc188c7p162535jsn887206772f31',
+            'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+    loading.classList.remove("d-none")
+    let api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`, options)
+    let response = await api.json()
+    allGames = response;
+
+    disblayData()
+    loading.classList.add("d-none")
+}
+function disblayData() {
+    let cartona = "";
+
+    for (let i = 0; i < allGames.length; i++) {
+        cartona += `
+       <div class="col-md-6 col-lg-3">
+            <div class="card bg-transparent h-100" role="button"  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails(${allGames[i].id})">
+                            <div class="card-img-top img-container">
+                                <img src="${allGames[i].thumbnail}" alt="Tarisland" class="img-fluid">
+                            </div>
+                            <div class="card-body text-center d-flex justify-content-between">
+                                <h6 class="fw-bold text-white">${allGames[i].title}</h6>
+                                <span class="badge-span ">Free</span>
+                            </div>
+                            <p class="card-text small text-center">
+                                ${allGames[i].short_description}
+                            </p>
+                            <footer class="card-footer d-flex justify-content-between">
+                                <span class="badge badge-color">${allGames[i].publisher.split(" " , 2).join( " ")}</span>
+                                <span class="badge badge-color">${allGames[i].platform}</span>
+                            </footer>
+            </div>
+        </div>
+       `
+    }
+    document.getElementById("games-row").innerHTML =cartona;
+}
+ */
+
 
 // displayData() {
     //     let cartona = "";
--- JS/detils.js
+++ JS/detils.js
@@ -1,3 +1,71 @@
+class GameDetails {
+    constructor() {
+        this.apiOptions = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': '8790f1dafamsh856b2e63bc188c7p162535jsn887206772f31',
+                'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+            }
+        };
+        this.allDetails = [];
+        this.loadingElement = document.getElementById("loading");
+        this.gameBodyElement = document.getElementById("game-body");
+        this.showDetails()
+    }
+
+    async showDetails(id) {
+        try {
+            this.showLoading();  
+            let apiResponse = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, this.apiOptions);
+            let response = await apiResponse.json();
+
+            this.allDetails = response;  
+
+            this.displayData(response);  
+        } catch (error) {
+            console.error("Error fetching game details:", error);
+        } finally {
+            this.hideLoading(); 
+        }
+    }
+
+    showLoading() {
+        this.loadingElement.classList.remove("d-none");
+    }
+
+    hideLoading() {
+        this.loadingElement.classList.add("d-none");
+    }
+
+    displayData(response) {
+        this.gameBodyElement.innerHTML = `
+            <div class="row" id="detailsContent">
+                <div class="col-md-4">
+                    <img src="${response.thumbnail}" class="img-fluid" alt="game photo">
+                </div>
+                <div class="col-md-8">
+                    <h3>Title: ${response.title}</h3>
+                    <p>Category: <span class="badge text-bg-info">${response.genre}</span></p>
+                    <p>Platform: <span class="badge text-bg-info">${response.platform}</span></p>
+                    <p>Status: <span class="badge text-bg-info">${response.status}</span></p>
+                    <p class="small">${response.description}</p>
+                    <a class="btn btn-outline-warning" target="_blank" href="${response.game_url}">Show Game</a>
+                </div>
+            </div>
+        `;
+    }
+}
+
+let gameDetails = new GameDetails();
+    // let gameId = 'game-id';  
+    // gameDetails.showDetails(gameId);
+
+
+
+
+
+/****************Comments********************/
+
 /* 
 
 let allDetails = []
@@ -93,66 +161,4 @@ async function showDetails(id) {
 // const gameDetails = new GameDetails();
 // gameDetails.showDetails('game-id');
 
-
-export class GameDetails {
-    constructor() {
-        this.apiOptions = {
-            method: 'GET',
-            headers: {
-                'x-rapidapi-key': '8790f1dafamsh856b2e63bc188c7p162535jsn887206772f31',
-                'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
-            }
-        };
-        this.allDetails = [];
-        this.loadingElement = document.getElementById("loading");
-        this.gameBodyElement = document.getElementById("game-body");
-        this.showDetails()
-    }
-
-    async showDetails(id) {
-        try {
-            this.showLoading();  
-            let apiResponse = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, this.apiOptions);
-            let response = await apiResponse.json();
-
-            this.allDetails = response;  
-
-            this.displayData(response);  
-        } catch (error) {
-            console.error("Error fetching game details:", error);
-        } finally {
-            this.hideLoading(); 
-        }
-    }
-
-    showLoading() {
-        this.loadingElement.classList.remove("d-none");
-    }
-
-    hideLoading() {
-        this.loadingElement.classList.add("d-none");
-    }
-
-    displayData(response) {
-        this.gameBodyElement.innerHTML = `
-            <div class="row" id="detailsContent">
-                <div class="col-md-4">
-                    <img src="${response.thumbnail}" class="img-fluid" alt="game photo">
-                </div>
-                <div class="col-md-8">
-                    <h3>Title: ${response.title}</h3>
-                    <p>Category: <span class="badge text-bg-info">${response.genre}</span></p>
-                    <p>Platform: <span class="badge text-bg-info">${response.platform}</span></p>
-                    <p>Status: <span class="badge text-bg-info">${response.status}</span></p>
-                    <p class="small">${response.description}</p>
-                    <a class="btn btn-outline-warning" target="_blank" href="${response.game_url}">Show Game</a>
-                </div>
-            </div>
-        `;
-    }
-}
-
-// let gameDetails = new GameDetails();
-    // let gameId = 'game-id';  
-    // gameDetails.showDetails(gameId);
 
